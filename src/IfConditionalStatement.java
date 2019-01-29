762616cbb079f30307c2fa387bc45523d3a1e2fa
monkARC <monkarc59@gmail.com>
2019-01-30 01:30:26 +0530

    [break][While.. if.. Break statement]

--- src/IfConditionalStatement.java
+++ src/IfConditionalStatement.java
@@ -3,16 +3,18 @@ import com.sun.org.apache.xpath.internal.SourceTree;
 public class IfConditionalStatement {
     public static void main(String[] args) {
 
-        int myInt = 20;
+        int loop = 0;
 
-        if (myInt < 10) {
-            System.out.println("Yes, It's true!");
-        }
-        else if(myInt > 30){
-            System.out.println("No, It's false");
-        }
-        else {
-            System.out.println("None of the above.");
+        while(true){
+            System.out.println("Looping: "+ loop);
+
+            if(loop == 5){
+                break;
+            }
+
+            loop++;
+
+            System.out.println("Running");
         }
     }
 }
