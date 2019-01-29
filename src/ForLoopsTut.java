4bef494013273af096f62fc1bc971246cc50e89a
monkARC <monkarc59@gmail.com>
2019-01-30 01:04:43 +0530

    [ForLoop]for(initializer,condition,incremental)

--- src/ForLoopsTut.java
+++ src/ForLoopsTut.java
@@ -1,7 +1,9 @@
 public class ForLoopsTut {
     public static void main(String[] args) {
 
-        for(;;){
+        //for(initializer,condition,incremental){
+        // }
+        for(int i=0; i < 5; i++){ //i++ is shorthand notation of i=i+1
             System.out.println("Hello");
         }
     }
