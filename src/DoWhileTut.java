57d8ffebbdd636671cd2f45b556addf638ae7fef
monkARC <monkarc59@gmail.com>
2019-01-30 01:58:03 +0530

    [DoWhileLoop][1 loop executes before condition is checked]

--- src/DoWhileTut.java
+++ src/DoWhileTut.java
@@ -6,6 +6,7 @@ public class DoWhileTut {
         // to get input
         Scanner scanner = new Scanner(System.in);
 
+        /*
         System.out.println("Enter a number:");
         int value = scanner.nextInt();
 
@@ -13,7 +14,14 @@ public class DoWhileTut {
             System.out.println("Enter a number: ");
             value = scanner.nextInt();
         }
+        */
 
+        int value = 0;
+        do {
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+        while(value != 5);
         System.out.println("Got 5!");
     }
 }
