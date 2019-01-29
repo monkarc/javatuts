e1f5006ffe42f62dd9f694f86508cbdffe83d78c
monkARC <monkarc59@gmail.com>
2019-01-30 00:20:11 +0530

    [variables][8 primitive types]

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,26 @@
 public class Application {
 
     public static void main(String[] args){
-        int myNumber=88;
+        int myNumber = 88; // 32-bit
+        short myShort = 847; // 16-bit
+        long myLong = 9797; //64-bit
+
+        double myDouble = 7.2354;
+        float myFloat = 324.3f; //shorter version of Double
+                                // with mentioning f in end
+
+        char myChar = 'y'; //unicode character (16-bit)
+        boolean myBoolean = false;
+
+        byte myByte = 127; //8-bit
 
         System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
     }
 }
