27f9221e18219b19c9c845c71ed0109e51e7f159
monkARC <monkarc59@gmail.com>
2019-01-30 01:23:51 +0530

    [If.. else condition]

--- src/IfConditionalStatement.java
+++ src/IfConditionalStatement.java
@@ -1,17 +1,13 @@
 public class IfConditionalStatement {
     public static void main(String[] args) {
-        //conditional checkers
-        // 1. >
-        // 2. <
-        // 3. ==
-        // 4. !=
 
-        if (4 == 5) {
+        int myInt = 20;
+
+        if (myInt < 10) {
             System.out.println("Yes, It's true!");
         }
-
-        if (4 == 4){
-            System.out.println("Yes, It's 4 is equal to 4!");
+        else{
+            System.out.println("No, It's false");
         }
     }
 }
