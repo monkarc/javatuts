5d0e230fac8f16b84c8853f4d63371f4d19ddabc
monkARC <monkarc59@gmail.com>
2019-01-30 01:52:31 +0530

    [Do while loop][example use-case without do..while]

---
+++ src/DoWhileTut.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DoWhileTut {
+    public static void main(String[] args) {
+
+        // to get input
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int value = scanner.nextInt();
+
+        while(value != 5){
+            System.out.println("Enter a number: ");
+            value = scanner.nextInt();
+        }
+
+        System.out.println("Got 5!");
+    }
+}
