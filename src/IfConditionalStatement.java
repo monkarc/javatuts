af347d95605431aad94b39f8c691bb1f02c6eaa7
monkARC <monkarc59@gmail.com>
2019-01-30 01:26:48 +0530

    [If.. else.. if.. else condition statement]

--- src/IfConditionalStatement.java
+++ src/IfConditionalStatement.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class IfConditionalStatement {
     public static void main(String[] args) {
 
@@ -6,8 +8,11 @@ public class IfConditionalStatement {
         if (myInt < 10) {
             System.out.println("Yes, It's true!");
         }
-        else if(myInt < 30){
+        else if(myInt > 30){
             System.out.println("No, It's false");
+        }
+        else {
+            System.out.println("None of the above.");
         }
     }
 }
