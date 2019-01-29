24c5983c1f8b812a83957dd615fc105f385f8f6f
monkARC <monkarc59@gmail.com>
2019-01-30 01:22:14 +0530

    [If condition]

---
+++ src/IfConditionalStatement.java
@@ -0,0 +1,17 @@
+public class IfConditionalStatement {
+    public static void main(String[] args) {
+        //conditional checkers
+        // 1. >
+        // 2. <
+        // 3. ==
+        // 4. !=
+
+        if (4 == 5) {
+            System.out.println("Yes, It's true!");
+        }
+
+        if (4 == 4){
+            System.out.println("Yes, It's 4 is equal to 4!");
+        }
+    }
+}
