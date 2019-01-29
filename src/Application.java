964d823d3c531aa7214475aee0f9eaec952d3c4b
monkARC <monkarc59@gmail.com>
2019-01-29 23:59:15 +0530

    [variables]int initialization in 2 steps

--- src/Application.java
+++ src/Application.java
@@ -1,6 +1,10 @@
 public class Application {
 
     public static void main(String[] args){
-        System.out.println("Hello World");
+        int myNumber;
+
+        myNumber = 88;
+
+        System.out.println(myNumber);
     }
 }
