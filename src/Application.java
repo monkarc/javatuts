f427767df39b9516c47005fa4dd0f7859c4ed4ca
monkARC <monkarc59@gmail.com>
2019-01-30 00:00:26 +0530

    [variables]int initialization in single step

--- src/Application.java
+++ src/Application.java
@@ -1,9 +1,7 @@
 public class Application {
 
     public static void main(String[] args){
-        int myNumber;
-
-        myNumber = 88;
+        int myNumber=88;
 
         System.out.println(myNumber);
     }
